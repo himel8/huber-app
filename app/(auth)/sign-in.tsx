870f3ea0c29564af459be3bc2da45c04bb608f09
himel8 <himelkhan290@gmail.com>
2024-08-27 03:19:00 +0600

    sign in screen has been added

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,63 @@
-/* eslint-disable prettier/prettier */
-
-import React from "react";
-import { Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSignInPress = async () => {};
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <ScrollView>
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 🖐
+          </Text>
+        </View>
+
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+
+          {/* OAuth */}
+          <OAuth />
+
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Don't have an account?</Text>
+            <Text className="text-primary-500">Sign UP</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
