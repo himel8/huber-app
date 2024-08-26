0a2ad222ea25e3ad09d14e69966f732943895e6a
himel8 <himelkhan290@gmail.com>
2024-08-27 03:15:16 +0600

    added the sign up screen

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
 import { Link } from "expo-router";
 import React, { useState } from "react";
@@ -53,6 +54,8 @@ const SignUp = () => {
           />
 
           {/* OAuth */}
+          <OAuth />
+
           <Link
             href="/sign-in"
             className="text-lg text-center text-general-200 mt-10"
