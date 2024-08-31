0595180bef053c5c6c9b97a164a134d4c8e7af01
himel8 <himelkhan290@gmail.com>
2024-08-31 10:48:05 +0600

    added the react map

---
+++ components/Map.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+
+const Map = () => {
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <Text>Map</Text>
+    </MapView>
+  );
+};
+
+export default Map;
